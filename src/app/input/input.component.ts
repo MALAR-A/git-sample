94cd008edadf7ba6d6439c4c9b0aec839751d9f5
Godwin6 <godwinpearlson006@gmail.com>
2019-06-07 18:04:57 +0530

    input component added

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inputs',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent{
+
+}
